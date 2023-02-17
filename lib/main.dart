4c0367678013982e29c7d60689de8222c0d64416
jt express <paanjnt@gmail.com>
2023-02-17 15:52:23 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:map_exam/login_screen.dart';
+
+// import 'login_screen.dart';
+import 'home_screen.dart';
+// import 'edit_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'myFirst',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const LoginScreen(),
+      home: const LoginScreen(),
+      // home: const EditScreen(),
+    );
+  }
+}
